c8633c4570b6841bb9df2c834edd2ffff0b4f346
ehasanulh1 <ehasanulh1@gmail.com>
2023-03-06 10:01:37 +0600

    module finish

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+/*
+count
+per page
+current page
+*/
 
 //middleware
 app.use(express.json());
@@ -20,8 +26,21 @@ async function run() {
         const productCollection = client.db('emaJohn').collection('products');
 
         app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size)
             const query = {}
             const cursor = productCollection.find(query)
+            const products = await cursor.skip(page * size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        });
+
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => new ObjectId(id));
+            const query = { _id: { $in: objectIds } };
+            const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         })
